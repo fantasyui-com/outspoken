4ab220f17639d86a899afd1a6f41b04321e0d7fe
fantasyui-com <captain@fantasyui.com>
2019-04-20 10:10:31 -0400

    Drop ix in favor of Promises and Compiler/Generator

--- index.mjs
+++ index.mjs
@@ -1,30 +1,18 @@
 import * as Ix from 'ix';
 
-export default function outspoken(str) {
-    // console.log(`OUTSPOKEN ${str}`)
-    let list = str.split(/\n/)
+export default function outspoken(specification, options) {
 
-    let counter = 0;
-    async function* gen(list) {
 
-      for(let x = 0; x < list.length; x++){
-        let item = list[x];
-        yield new Promise(resolve => {
-          setTimeout(function() {
-            counter ++;
-            resolve(item);
-            //console.log("fast promise is done");
-          }, 2000);
-        });
+  specification.split(/\n/).forEach((line)=>{
 
-      }
+      registered.forEach((matcher)=>{
+        const match = XRegExp.exec(line, XRegExp(matcher.expression) );
+        if(match) {
+          result.push({name:matcher.event, data:match})
+        }
+      })
 
-    }
+    });
 
-    const results = Ix.AsyncIterable.from(gen(list))
-      .filter(x => true)
-      .map(x => x);
-
-      return results;
-
+  return results;
 }
