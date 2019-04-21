1da04888c4e32dad888556b0756bf908c9797d25
fantasyui-com <captain@fantasyui.com>
2019-04-21 08:44:50 -0400

    Initial Code Generation (require and function calls)

--- index.mjs
+++ index.mjs
@@ -1,10 +1,11 @@
 import XRegExp from 'xregexp';
 import generate from 'project-name-generator';
-import kebabCase from 'lodash/kebabcase';
+import kebabCase from 'lodash/kebabCase';
+import camelCase from 'lodash/camelCase';
 
 export default function outspoken(specification, options) {
 
-  const results = [];
+  const results = {list:[]};
 
   specification.split(/\n/).map(i => i.trim()).filter(i => i.length).forEach((line, index) => {
     const result = {};
@@ -25,18 +26,48 @@ export default function outspoken(specification, options) {
         result.module = kebabCase(matchPipe.text)
       }
 
-      results.push(result);
+      results.list.push(result);
 
     }
     else if (matchData) {
       //let content = matchData.data.split(/,/).map(i=>i.trim());
       const content = matchData.data;
-      results[results.length - 1][matchData.label] = content;
+      results.list[results.list.length - 1][matchData.label] = content;
       //console.log('@>>>>> ' + matchData.label + ':' + matchData.data)
     }
 
 
   });
+
+  let moduleImport = [];
+  let moduleRequire = [];
+  results.list.forEach(function(item){
+    moduleImport.push(`import ${camelCase(item.module)} from '${item.module}';`);
+    moduleRequire.push(`const ${camelCase(item.module)} = require(${item.module});`);
+
+  })
+  results.moduleImport = moduleImport.join('\n');
+  results.moduleRequire = moduleRequire.join('\n');
+
+  let alpha = ['const result = '];
+  let omega = [';'];
+  results.list.forEach(function(item){
+    alpha.push(`${camelCase(item.module)}(configuration, `);
+    omega.unshift(`)`);
+  })
+  results.terseCode = alpha.concat(omega).join('');
+
+  let verboseCode = [];
+  let previousContentName = 'null';
+  results.list.forEach(function(item){
+    let contentName = camelCase(item.module + '-response');
+    verboseCode.push(`// ${item.data}`);
+    verboseCode.push(`const ${contentName} = ${camelCase(item.module)}(configuration, ${previousContentName});\n`);
+    previousContentName = contentName;
+  })
+  results.verboseCode = verboseCode.join('\n');
+
+
   return results;
 
 }
