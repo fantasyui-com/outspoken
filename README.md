4c3c5ca3743e3782262c64ae2f6e56b7d59bf3dc
fantasyui-com <captain@fantasyui.com>
2019-04-23 09:43:03 -0400

    Enhanced Language Tokenizer and Statement Parser

--- README.md
+++ README.md
@@ -10,18 +10,29 @@ User interfaces are streamed into the DOM
 
 ## Example Program
 
-	Configuration:
-		Author: Meow
-		Program: dragonfly
-		Description: A simple dashboard
+### PROCEDURE DIVISION
 
+#### Main
 
-	Login
-		initialize-client-session: Prepare user session.
-		create-home-ui: Create base UI for the user.
-		populate-home: Stream in user widgets
+- Configure Server
+  * port: 8081
+- Validate configuration object using is-valid-object.
+  * fields: repository, commitMessage, isNpm
+  * dropPacket: true
+- Check if this is a valid github repository using is-github-repo.
+- Ensure local availability of git repository using git-get.
+- Update NPM Package using npm-update
+- Update NPM License field using npm-license-field
+  * license: GPL-3.0
+- Perform NPM Audit using npm-audit
+- Update LICENSE file using gpl-license
+- Update npm
+- Update github
+- Tweet about package update
 
-	Logout
-		destroy-client-session: Free up resources used by client session
-		redirect-home: Redirect browser to root of the domain
+#### Notification
+bork
 
+## Program License
+
+see ./LICENSE file
--- index.mjs
+++ index.mjs
@@ -3,71 +3,149 @@ import generate from 'project-name-generator';
 import kebabCase from 'lodash/kebabCase';
 import camelCase from 'lodash/camelCase';
 
-export default function outspoken(specification, options) {
+export default function outspoken({source, options}) {
 
-  const results = {list:[]};
+  const output = {
+    developmentStatus: {},
+    unitTests: {},
+    compile: {},
+    server: {},
+  };
 
-  specification.split(/\n/).map(i => i.trim()).filter(i => i.length).forEach((line, index) => {
-    const result = {};
+  const lineList = source.split(/\n/);
+  const sectionMatcher = /^ *(#{1,6}) *([^\n]+?) *(?:#+ *)?(?:\n+|$)/;
 
-    const matchPipe = XRegExp.exec(line, XRegExp(`^- *(?<text>.+)$`));
-    const matchModule = XRegExp.exec(line, XRegExp(`^- *.*using (?<name>[0-9a-z-]+)`));
-    const matchData = XRegExp.exec(line, XRegExp(`^\\* *(?<label>[a-zA-Z0-9-]+): *(?<data>.+)$`));
+  const state = {
+    name: '',
+    depth: 0,
+    path: [],
+  };
 
-    if (matchPipe) {
-      //result.type = 'pipe';
-      //result.label = currentLabel;
-      result.number = index;
-      result.data = matchPipe.text;
-      if (matchModule) {
-        result.module = matchModule.name;
-      }else{
-        //result.module = generate({ words: 3}).dashed;
-        result.module = kebabCase(matchPipe.text)
-      }
-
-      results.list.push(result);
-
+  const abstract = [];
+  lineList.forEach(function(line) {
+    let sectionMatch = sectionMatcher.exec(line);
+    if (sectionMatch) { // alter state
+      let about = {};
+      about.title = sectionMatch[2].trim();
+      about.depth = sectionMatch[1].length;
+      state.name = about.title;
+      state.depth = about.depth;
+      state.path[about.depth - 1] = about.title
+      state.path = state.path.slice(0, about.depth)
     }
-    else if (matchData) {
-      //let content = matchData.data.split(/,/).map(i=>i.trim());
-      const content = matchData.data;
-      results.list[results.list.length - 1][matchData.label] = content;
-      //console.log('@>>>>> ' + matchData.label + ':' + matchData.data)
+    else { // log data
+      if (line.trim()) {
+        const previous = abstract[abstract.length - 1];
+        const current = {
+          depth: state.depth,
+          path: state.path.slice(0),
+          data: [line]
+        };
+        if ((previous) && (previous.path.join() === current.path.join())) {
+          previous.data.push(line);
+        }
+        else {
+          abstract.push(current);
+        }
+
+      } // if line
     }
-
-
   });
 
-  let moduleImport = [];
-  let moduleRequire = [];
-  results.list.forEach(function(item){
-    moduleImport.push(`import ${camelCase(item.module)} from '${item.module}';`);
-    moduleRequire.push(`const ${camelCase(item.module)} = require(${item.module});`);
+
+  // Iterate over abstract syntax extrtacting PROCEDURE DIVISION items
+  const content = [];
+  abstract.forEach(function(item) {
+    if (item.path.includes('PROCEDURE DIVISION')) {
+      content.push(item);
+    }
+  })
+
+  // Interpret special lines
+  content.forEach(function(item) {
+
+    const results = [];
+
+    item.data.map(i => i.trim()).filter(i => i.length)
+    .forEach((line, index) => {
+
+        const result = {options:{}};
+
+        const matchPipe = XRegExp.exec(line, XRegExp(`^- *(?<text>.+)$`));
+        const matchModule = XRegExp.exec(line, XRegExp(`^- *.*using (?<name>[0-9a-z-]+)`));
+        const matchData = XRegExp.exec(line, XRegExp(`^\\* *(?<label>[a-zA-Z0-9-]+): *(?<data>.+)$`));
+
+        if (matchPipe) {
+          //result.type = 'pipe';
+          //result.label = currentLabel;
+          result.number = index;
+          result.data = matchPipe.text;
+          if (matchModule) {
+            result.module = matchModule.name;
+          }else{
+            //result.module = generate({ words: 3}).dashed;
+            result.module = kebabCase(matchPipe.text)
+          }
+
+          results.push(result);
+
+        }
+        else if (matchData) {
+          //let content = matchData.data.split(/,/).map(i=>i.trim());
+          const content = matchData.data;
+          results[results.length - 1].options[matchData.label] = content;
+          //console.log('@>>>>> ' + matchData.label + ':' + matchData.data)
+        }
+
+
+
+
+
+    });
+
+    item.data = results;
 
   })
-  results.moduleImport = moduleImport.join('\n');
-  results.moduleRequire = moduleRequire.join('\n');
 
-  let alpha = ['const result = '];
-  let omega = [';'];
-  results.list.forEach(function(item){
-    alpha.push(`${camelCase(item.module)}(configuration, `);
-    omega.unshift(`)`);
-  })
-  results.terseCode = alpha.concat(omega).join('');
+  //
 
-  let verboseCode = [];
-  let previousContentName = 'null';
-  results.list.forEach(function(item){
-    let contentName = camelCase(item.module + '-response');
-    verboseCode.push(`// ${item.data}`);
-    verboseCode.push(`const ${contentName} = ${camelCase(item.module)}(configuration, ${previousContentName});\n`);
-    previousContentName = contentName;
-  })
-  results.verboseCode = verboseCode.join('\n');
+  console.log(JSON.stringify(content, null, ' '));
+
+// TODO: CODE GENERATION that takes multiple programs under consideration, this will occur on DIVISION LEVEL
 
 
-  return results;
+  // let moduleImport = [];
+  // let moduleRequire = [];
+  // results.list.forEach(function(item){
+  //   moduleImport.push(`import ${camelCase(item.module)} from '${item.module}';`);
+  //   moduleRequire.push(`const ${camelCase(item.module)} = require(${item.module});`);
+  //
+  // })
+  // results.moduleImport = moduleImport.join('\n');
+  // results.moduleRequire = moduleRequire.join('\n');
+  //
+  // let alpha = ['const result = '];
+  // let omega = [';'];
+  // results.list.forEach(function(item){
+  //   alpha.push(`${camelCase(item.module)}(configuration, `);
+  //   omega.unshift(`)`);
+  // })
+  // results.terseCode = alpha.concat(omega).join('');
+  //
+  // let verboseCode = [];
+  // let previousContentName = 'null';
+  // results.list.forEach(function(item){
+  //   let contentName = camelCase(item.module + '-response');
+  //   verboseCode.push(`// ${item.data}`);
+  //   verboseCode.push(`const ${contentName} = ${camelCase(item.module)}(configuration, ${previousContentName});\n`);
+  //   previousContentName = contentName;
+  // })
+  // results.verboseCode = verboseCode.join('\n');
+
+
+
+
+  return output;
+
 
 }
